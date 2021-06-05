ca33a57cab55e7637c2429fed5ecaee1e488ffa3
shrikant.upadhyay <shrikant1997upadhyay.gmail.com>
2021-06-05 17:01:37 +0530

    Added Coors.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ require("./db/mongoose");
 const userRouter = require("./routers/users");
 const taskRouter = require("./routers/tasks");
 const app = express();
+var cors = require("cors");
 const port = process.env.PORT;
 const multer = require("multer");
 const upload = multer({ dest: "images" });
 app.post("/upload", upload.single("upload"), async (req, res) => {
   res.send();
+});
+app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
 });
 
 app.use(express.json());
